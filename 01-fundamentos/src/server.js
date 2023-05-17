9748e413ba6a96701b3a397a5077992069884157
Thiago F.Ramos <th.framos@gmail.com>
2023-05-16 22:32:19 -0300

    criando banco de dados

--- 01-fundamentos/src/server.js
+++ 01-fundamentos/src/server.js
@@ -1,27 +1,34 @@
 import http from 'node:http';
+import {randomUUID} from 'node:crypto'
+import { Database } from './database.js';
 import { json } from './middlewares/json.js';
 
 const users = [];
+const database = new Database();
 
 const server = http.createServer(async (request, response) => {
+
+
 
     const { method, url } = request
 
     await json(request, response)
 
     if (method === 'GET' && url === '/users') {
+        const data = database.select('users')
+
         return response
-            .end(JSON.stringify(users))
+            .end(JSON.stringify(data))
     }
 
     if (method === 'POST' && url === '/users') {
         const { name, email } = request.body
-
-        users.push({
-            id: 1,
+        const data = {
+            id: randomUUID(),
             name,
             email
-        })
+        }
+        database.insert('users', data)
         return response.writeHead(201).end()
     }
     return response.writeHead(404).end()
